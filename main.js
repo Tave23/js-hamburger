ffe38c1f19ab69ac8bfddd236fb7a9129b127a71
Davide Tavella <tavella01@gmail.com>
2021-10-22 17:34:17 +0200

    fine esercizio

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const hamburger = document.querySelector ('.hamburger-menu');
+const hamburger = document.querySelector ('.header-right > a');
 const navMenu = document.querySelector ('.header-right');
+const closeMenu = document.querySelector ('.hamburger-menu > a');
 
+hamburger.addEventListener('click', function() {
+   console.log('abbiamo cliccato hamburger menu');
 
-hamburger.addEventListener('click', () => {
-   hamburger.classList.toggle('active');
-   navMenu.classList.toggle("active");
+   const elementToShow = document.querySelector('.hamburger-menu');
+   elementToShow.classList.add('active');
+}); 
+
+closeMenu.addEventListener('click', function()  {
+   console.log('abbiamo chiuso la page');
+
+   const Menuchiuso = document.querySelector('.hamburger-menu');
+   Menuchiuso.classList.remove('active');
 })
