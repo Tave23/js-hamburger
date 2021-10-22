ad15b84bc76d2035d341605041fbf37c19d2aa84
Davide Tavella <tavella01@gmail.com>
2021-10-22 18:16:23 +0200

    bonus - Home all'interno del "hamburger-menu" lo fa richiudere

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector ('.header-right > a');
 const navMenu = document.querySelector ('.header-right');
 const closeMenu = document.querySelector ('.hamburger-menu > a');
+const linkCloseMenu = document.querySelector ('.hamburger-menu > ul > li > a')
 
 hamburger.addEventListener('click', function() {
    console.log('abbiamo cliccato hamburger menu');
@@ -14,4 +15,11 @@ closeMenu.addEventListener('click', function()  {
 
    const Menuchiuso = document.querySelector('.hamburger-menu');
    Menuchiuso.classList.remove('active');
+})
+
+linkCloseMenu.addEventListener('click', function() {
+   console.log('abbiamo cliccato i link');
+
+   const bonusMenuchiuso = document.querySelector('.hamburger-menu');
+   bonusMenuchiuso.classList.remove('active');
 })
